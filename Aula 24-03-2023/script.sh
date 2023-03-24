06ee7f13452d5bc44cb8101c3277aeb25975f44e
Thalles Canela <thalles_tsc@hotmail.com>
2023-03-24 19:40:27 -0300

    Update script.sh

--- Aula 24-03-2023/script.sh
+++ Aula 24-03-2023/script.sh
@@ -11,3 +11,15 @@ apt-get install docker-ce docker-ce-cli containerd.io
 docker -version
 
 systemctl status docker
+
+# Instalação do Portainer Server
+# Criando um volume
+docker volume create portainer_data
+# Instalando o Portainer em um container
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+
+# Acessando o Portainer
+# https://localhost:9443
+
+# Primeiro Acesso
+# https://docs.portainer.io/start/install/server/setup
