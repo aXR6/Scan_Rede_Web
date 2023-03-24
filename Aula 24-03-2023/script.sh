03393a878fd522b14696c0832b9d4908441fd448
Thalles Canela <thalles_tsc@hotmail.com>
2023-03-24 20:20:47 -0300

    Update script.sh

--- Aula 24-03-2023/script.sh
+++ Aula 24-03-2023/script.sh
@@ -16,7 +16,7 @@ systemctl status docker
 # Criando um volume
 docker volume create portainer_data
 # Instalando o Portainer em um container
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
 # Acessando o Portainer
 # https://localhost:9443
