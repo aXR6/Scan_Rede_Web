396aa11e2f87b6fcd3ddbc80f7c777e5a7f35a44
root <thalles_tsc@hotmail.com>
2023-03-25 11:58:39 -0300

    Instalar o Docker, Portainer e Layout para subir o NextCloud e MariaDB

--- 1 - Install_Docker_Portainer_NextCloud_MariaDB_/script.sh
+++ 1 - Install_Docker_Portainer_NextCloud_MariaDB_/script.sh
@@ -2,19 +2,14 @@
 
 echo -e "\033[1;31m[----------------------------------------------------------- PREPARANDO O AMBIENTE -----------------------------------------------------------] \033[0m \033[1;31m \033[0m"
 apt update
-
 apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 echo -e "\033[1;31m[----------------------------------------------------------- PREPARANDO O AMBIENTE -----------------------------------------------------------] \033[0m \033[1;31m \033[0m"
 
 echo -e "\033[1;31m[----------------------------------------------------------- INSTALANDO O DOCKER -----------------------------------------------------------] \033[0m \033[1;31m \033[0m"
 apt-get update
-
 apt-get -y install docker-ce docker-ce-cli containerd.io
-
 docker -version
 echo -e "\033[1;31m[----------------------------------------------------------- INSTALANDO O DOCKER -----------------------------------------------------------] \033[0m \033[1;31m \033[0m"
 
