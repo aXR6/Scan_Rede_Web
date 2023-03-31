8bd330c111481c149b4d94ddc81f61b4cf146233
root <thalles_tsc@hotmail.com>
2023-03-31 15:58:50 -0300

    Atualização

--- 1 - Install_Docker_Portainer_NextCloud_MariaDB_/script.sh
+++ 1 - Install_Docker_Portainer_NextCloud_MariaDB_/script.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
-ip_address=$(ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
- 
+
 echo -e "\033[1;31m--------------------- PREPARANDO O AMBIENTE --------------------- \033[0m \033[1;31m \033[0m"
-apt update && apt -y install net-tools
-apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
-echo -e "\033[1;31m--------------------- PREPARANDO O AMBIENTE --------------------- \033[0m \033[1;31m \033[0m"
- 
-echo -e "\033[1;31m--------------------- INSTALANDO O DOCKER --------------------- \033[0m \033[1;31m \033[0m"
+#!/bin/bash
+
+# Atualiza o sistema operacional
+apt-get update && apt-get upgrade -y
+
+# Instala os pacotes necessários
+apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+# Adiciona a chave GPG oficial do Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+
+# Adiciona o repositório oficial do Docker
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+
+# Atualiza novamente os pacotes
 apt-get update
-apt-get -y install docker-ce docker-ce-cli containerd.io
-docker -version
-echo -e "\033[1;31m--------------------- INSTALANDO O DOCKER --------------------- \033[0m \033[1;31m \033[0m"
- 
-echo -e "\033[1;31m--------------------- INSTALANDO O PORTAINER --------------------- \033[0m \033[1;31m \033[0m"
+
+# Instala o Docker CE, Docker CE CLI e o Containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Adiciona o usuário atual ao grupo do Docker
+usermod -aG docker $USER
+
+# Configura o Portainer CE
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
-echo -e "\033[1;31m--------------------- INSTALANDO O PORTAINER --------------------- \033[0m \033[1;31m \033[0m"
- 
-echo -e "\033[1;31m--------------------- FORMA DE ACESSAR --------------------- \033[0m \033[1;31m \033[0m"
-echo -e "\033[1;31m [ Acessando o Portainer: https://$ip_address:9443 ] \033[0m \033[1;31m \033[0m"
- 
-echo -e "\033[1;31m [ Primeiro Acesso ] \033[0m \033[1;31m \033[0m"
-echo -e "\033[1;31m [ https://docs.portainer.io/start/install/server/setup ] \033[0m \033[1;31m \033[0m"
-echo -e "\033[1;31m--------------------- FORMA DE ACESSAR --------------------- \033[0m \033[1;31m \033[0m"
+docker run -d -p 9000:9000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+# Imprimir instruções finais
+echo ""
+echo "Docker e Portainer foram instalados com sucesso!"
+echo "Para usar o Docker sem precisar usar sudo, faça logout e login novamente."
+echo "Acesse o Portainer em http://localhost:9000."
+echo -e "\033[1;31m--------------------- PREPARANDO O AMBIENTE --------------------- \033[0m \033[1;31m \033[0m"
